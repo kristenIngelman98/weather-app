e02b378ff31d33459ff29a947fb3c0db0030492d
Kristen <kristeningelman@hotmail.com>
2022-11-14 12:56:03 -0800

    changed units to Fahrenheit

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const key = process.env.ACCESS_KEY
-const url = `http://api.weatherstack.com/current?access_key=${key}&query=37.8267,-122.42233`
+const url = `http://api.weatherstack.com/current?access_key=${key}&query=37.8267,-122.42233&units=f`
 
-request({ url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({ url: url, json: true}, (error, response) => {
+    // const data = JSON.parse(response.body)
+    // console.log(response.body.current)
+    const data = response.body.current
+    console.log(`It is currently ${data.temperature} degrees out and ${data.weather_descriptions}. There is a ${data.precip}% chance of rain.`)
 })
