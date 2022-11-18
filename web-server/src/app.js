5217250568fa15fc0236f2dcf24ea28390afed67
Kristen <kristeningelman@hotmail.com>
2022-11-18 15:08:19 -0800

    added html files

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,20 +1,16 @@
+const path = require('path')
 const express = require('express')
 
 const app = express() 
+console.log(__dirname)
+console.log(path.join(__dirname, '../public'))
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.send('<h1>Weather</h1>')
-})
-
-app.get('/help', (req, res) => {
-    res.send({
-        name: 'Kristen',
-        age: 24
-    })
-})
-
-app.get('/about', (req, res) => {
-    res.send('<h1>About</h1>')
 })
 
 app.get('/weather', (req, res) => {
