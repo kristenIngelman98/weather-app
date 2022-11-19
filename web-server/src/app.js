e890fbc9ad3078af96413ab402037e3e8498bde0
Kristen <kristeningelman@hotmail.com>
2022-11-19 13:46:36 -0800

    added hbs template pages with dynamic content

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,15 +2,24 @@ const path = require('path')
 const express = require('express')
 
 const app = express() 
-console.log(__dirname)
-console.log(path.join(__dirname, '../public'))
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
-    res.send('<h1>Weather</h1>')
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Kristen Ingelman'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Kristen Ingelman'
+    })
 })
 
 app.get('/weather', (req, res) => {
