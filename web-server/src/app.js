d57238a577c91f7ea3b57c64fbf3cb4691b73719
Kristen <kristeningelman@hotmail.com>
2022-11-21 15:52:55 -0800

    updating folder structure

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 const express = require('express')
 const hbs = require('hbs')
 
@@ -40,9 +42,47 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location }) => {
+        if (error) {
+            return res.send({ error })
+        } 
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+
+    // res.send({
+    //     forecast: 'It is snowing',
+    //     location: 'Victoria',
+    //     address: req.query.address
+    // })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query.search)
     res.send({
-        forecast: 'Sunny',
-        location: 'Victoria, BC'
+        products: []
     })
 })
 
