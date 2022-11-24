d84a0e0baf37f8c4d303889a85c9afbe437ba096
Kristen <kristeningelman@hotmail.com>
2022-11-24 14:02:36 -0800

    heroku port set

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,10 @@
 // client side js
 console.log('client side js file is loaded!')
 
-fetch('https://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
-})
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const message1 = document.getElementById('message-1')
 const message2 = document.getElementById('message-2')
-
-
 
 
 weatherForm.addEventListener('submit', (e) => {
@@ -24,7 +14,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     console.log(location)
 
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    fetch(`/weather?address=${location}`).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             // console.log(data.error)
