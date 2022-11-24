258bd9c45bcba6fb95920b542c0dcd49de914764
Kristen <kristeningelman@hotmail.com>
2022-11-24 13:28:23 -0800

    formatting

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -7,7 +7,6 @@ const geocode = (address, callback) => {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${token}`
 
     request({ url, json: true }, (error, { body }) => {
-        // console.log(body)
         if (error) {
             callback('Unable to connect to location services!', undefined)
         } else if (body.features.length === 0) {
